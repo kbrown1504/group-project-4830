f216f230855e1841ddb2c75be9f9aba939952fa8
jcwga <jcwga@LAPTOP-8NFE14MD>
2021-11-04 12:46:51 -0500

    Split parser into 4 discrete methods

--- group-project-4830/src/main/java/datamodels/DataParser.java
+++ group-project-4830/src/main/java/datamodels/DataParser.java
@@ -4,57 +4,54 @@ import java.util.ArrayList;
 
 public class DataParser {
 	
-	//Hand in result set and a subclass DataModel to identify type
-	public static <T extends DataModel> ArrayList<? extends DataModel> parse(ResultSet res, T master) throws SQLException
+	public static ArrayList<Account> parseAccount(ResultSet res) throws SQLException
 	{
-		ArrayList<? extends DataModel> ret = new ArrayList<DataModel>();
-		switch(master.getClass().toString()) 
+		ArrayList<Account> temp = new ArrayList<Account>();
+		while (res.next())
 		{
-			case "Account":
-				ArrayList<Account> tempA = new ArrayList<Account>();
-				while (res.next())
-				{
-					Account t1 = new Account(res.getInt("ID"), res.getString("Username"), res.getString("Email"), res.getString("Password"));
-					tempA.add(t1);
-				}
-				ret = tempA;
-				break;
-			case "BookListing": //BookListing code
-				ArrayList<BookListing> tempB = new ArrayList<BookListing>();
-				while (res.next())
-				{
-					BookListing t1 = new BookListing(res.getInt("ID"), res.getString("Title"), res.getString("Author"), 
-							res.getDouble("ISBN"), res.getDouble("Price"), res.getInt("Seller"), res.getInt("Condition"),
-							res.getString("AddInfo)"));
-					tempB.add(t1);
-				}
-				ret = tempB;
-				break;
-			case "Order": //Order code
-				ArrayList<Order> tempC = new ArrayList<Order>();
-				while (res.next())
-				{
-					Order t1 = new Order(res.getInt("ID"), res.getInt("BookID"), res.getInt("SellerID"), res.getInt("BuyerID"),
-							res.getString("ShippingAddress"));
-					tempC.add(t1);
-				}
-				ret = tempC;
-				break;
-			case "Review": //Review code
-				ArrayList<Review> tempD = new ArrayList<Review>();
-				while (res.next())
-				{
-					Review t1 = new Review(res.getInt("ID"), res.getInt("SellerID"), res.getInt("BuyerID"), res.getInt("Rating"),
-							res.getString("ReviewText"));
-					tempD.add(t1);
-				}
-				ret = tempD;
-				break;
-			default: //Error case
-				break;
+			Account t1 = new Account(res.getInt("ID"), res.getString("Username"), res.getString("Email"), res.getString("Password"));
+			temp.add(t1);
 		}
-		return ret;
+		return temp;
 	}
+	
+	public static ArrayList<BookListing> parseBookListing(ResultSet res) throws SQLException
+	{
+		ArrayList<BookListing> temp = new ArrayList<BookListing>();
+		while (res.next())
+		{
+			BookListing t1 = new BookListing(res.getInt("ID"), res.getString("Title"), res.getString("Author"), 
+					res.getDouble("ISBN"), res.getDouble("Price"), res.getInt("Seller"), res.getInt("Condition"),
+					res.getString("AddInfo)"));
+			temp.add(t1);
+		}
+		return temp;
+	}
+	
+	public static ArrayList<Order> parseOrder(ResultSet res) throws SQLException
+	{
+		ArrayList<Order> temp = new ArrayList<Order>();
+		while (res.next())
+		{
+			Order t1 = new Order(res.getInt("ID"), res.getInt("BookID"), res.getInt("SellerID"), res.getInt("BuyerID"),
+					res.getString("ShippingAddress"));
+			temp.add(t1);
+		}
+		return temp;
+	}
+	
+	public static ArrayList<Review> parseReview(ResultSet res) throws SQLException
+	{
+		ArrayList<Review> temp = new ArrayList<Review>();
+		while (res.next())
+		{
+			Review t1 = new Review(res.getInt("ID"), res.getInt("SellerID"), res.getInt("BuyerID"), res.getInt("Rating"),
+					res.getString("ReviewText"));
+			temp.add(t1);
+		}
+		return temp;
+	}
+
 	
 	
 }
