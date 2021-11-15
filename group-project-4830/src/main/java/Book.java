dc912b618970cfd7edc222bd8a7b2d41befb1341
MoistHugs <griffinj3lly@gmail.com>
2021-11-14 22:22:35 -0600

    FINALLY finished the Book Listing/Detail page

--- group-project-4830/src/main/java/Book.java
+++ group-project-4830/src/main/java/Book.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import datamodels.BookListing;
+
 /**
  * Servlet implementation class Book
  */
@@ -34,7 +36,15 @@ public class Book extends HttpServlet {
 		
 		String i = request.getParameter("id");
 		int id = Integer.parseInt(i);
+		BookListing test1 = new BookListing(0, -1, "Software Engineering", "Ian Sommerville", 9781292096131.0, 40.00, 0, 1, "test info");
 		//TODO: Need to grab book by ID from DB
+		
+		request.setAttribute("title", test1.getTitle());
+		request.setAttribute("author", test1.getAuthor());
+		request.setAttribute("isbn", test1.getISBN());
+		request.setAttribute("price", test1.getPrice());
+		request.setAttribute("seller", "John Doe");
+		request.setAttribute("info", test1.getInfo());
 		
 		RequestDispatcher view = request.getRequestDispatcher("book.jsp");
 		view.forward(request, response);
