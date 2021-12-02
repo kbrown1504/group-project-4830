175dea3d3ee02c4a241a4613b81f8bf6dd75ad9f
kbrown1504 <keegqbrown@gmail.com>
2021-12-02 11:56:42 -0600

    Fixing Redirect on Login

--- group-project-4830/src/main/java/Login.java
+++ group-project-4830/src/main/java/Login.java
@@ -52,14 +52,15 @@ public class Login extends HttpServlet {
             if (user != null) {
                 HttpSession session = request.getSession();
                 session.setAttribute("user", user);
-                destPage = "home.jsp";
+                destPage = "home";
+                response.sendRedirect("home");
             } else {
                 String message = "Invalid email/password";
                 request.setAttribute("message", message);
+                
+                RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+                dispatcher.forward(request, response);
             }
-             
-            RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
-            dispatcher.forward(request, response);
              
         } catch (SQLException ex) {
             throw new ServletException(ex);
